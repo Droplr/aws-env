15fa728a642e95a936aaa16317fc8282a3251f0c
Wouter de Geus <wdegeus@mirabeau.nl>
2018-03-13 14:09:34 +0100

    Fix for https://github.com/Droplr/aws-env/issues/13

--- aws-env.go
+++ aws-env.go
@@ -57,6 +57,7 @@ func PrintExportParameter(path string, parameter *ssm.Parameter) {
 
 	env := strings.Trim(name[len(path):], "/")
 	value = strings.Replace(value, "\n", "\\n", -1)
+	value = strings.Replace(value, "'", "\\'", -1)
 
 	fmt.Printf("export %s=$'%s'\n", env, value)
 }
